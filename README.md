fb5aeb88a7459f888ce8e307efbb45c141e8274d
aleeeexcool <ramzhev.sasha@outlook.com>
2023-04-13 22:31:38 +0300

    new project create

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # carrots_miner
+
+A carrot miner is one of the usual representatives of the Pay2Earn games - you buy carrot seeds, grow them, then sell them and buy more carrot seeds to grow more carrots and sell even more and so on and so on...
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ scripts/deploy.js
@@ -0,0 +1,15 @@
+const hre = require("hardhat");
+
+async function main() {
+  const CarrotsMiner = await hre.ethers.getContractFactory("CarrotsMiner");
+  const miner = await CarrotsMiner.deploy();
+
+  await miner.deployed();
+
+  console.log(`Contract address: ${miner.address}`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});

9ab4fd047783d017920bdbdcefc2cb19cd8d183a
aleeeexcool <ramzhev.sasha@outlook.com>
2023-04-28 20:32:25 +0300

    config update

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,9 +11,9 @@ module.exports = {
       }
   },
   networks: {
-    goerli: {
-      url: "",
-      accounts: []
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/iHoA18-q70fm_n_FbKynEEiTU_joWlTh",
+      accounts: [`d2536bf1124cba021fdaa9219963e90546b5f86dd46f16ad1cd49f8b4b1ef4db`]
     }
   }
 };

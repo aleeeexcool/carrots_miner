b615ee67a41eaa4e97e57f3b180e056158228756
aleeeexcool <ramzhev.sasha@outlook.com>
2023-05-05 15:34:19 +0300

    deploy script update

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,16 +1,63 @@
-const hre = require("hardhat");
+const hre = require('hardhat');
+const ethers = hre.ethers;
+const fs = require('fs');
+const path = require('path');
 
 async function main() {
-  const CarrotsMiner = await hre.ethers.getContractFactory("CarrotsMiner");
-  console.log("Deploying...");
-  const miner = await CarrotsMiner.deploy();
-  await miner.deployed();
-  console.log(`Contract address: ${miner.address}`);
+  if (network.name === "hardhat") {
+    console.warn(
+      "You are trying to deploy a contract to the Hardhat Network, which" +
+      "gets automatically created and destroyed every time. Use the Hardhat" +
+      " option '--network localhost'"
+    );
+  }
+
+  const [deployer] = await ethers.getSigners()
+
+  console.log("Deploying with", await deployer.getAddress())
+
+  const DutchAuction = await ethers.getContractFactory("DutchAuction", deployer)
+  const auction = await DutchAuction.deploy(
+    ethers.utils.parseEther('2.0'),
+    1,
+    "New NFT"
+  )
+  await auction.deployed()
+
+  saveFrontendFiles({
+    DutchAuction: auction
+  })
 }
 
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+function saveFrontendFiles(contracts) {
+  const contractsDir = path.join(__dirname, '/..', 'front/contracts')
 
-//0x048e76d7e243dff27c78d380c5323ab65bb5155b
+  if(!fs.existsSync(contractsDir)) {
+    fs.mkdirSync(contractsDir)
+  }
+
+  Object.entries(contracts).forEach((contract_item) => {
+    const [name, contract] = contract_item
+
+    if(contract) {
+      fs.writeFileSync(
+        path.join(contractsDir, '/', name + '-contract-address.json'),
+        JSON.stringify({[name]: contract.address}, undefined, 2)
+      )
+    }
+
+    const ContractArtifact = hre.artifacts.readArtifactSync(name)
+
+    fs.writeFileSync(
+      path.join(contractsDir, '/', name + ".json"),
+      JSON.stringify(ContractArtifact, null, 2)
+    )
+  })
+}
+
+ main()
+   .then(() => process.exit(0))
+   .catch((error) => {
+     console.error(error)
+     process.exit(1)
+   })

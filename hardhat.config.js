4e5a5bf01ffb2d0d065b9c7f727497e6baa7dede
aleeeexcool <ramzhev.sasha@outlook.com>
2023-06-01 22:44:46 +0300

    verify uninstall

--- hardhat.config.js
+++ hardhat.config.js
@@ -13,11 +13,11 @@ module.exports = {
   },
   networks: {
     sepolia: {
-      url: "https://eth-sepolia.g.alchemy.com/v2/iHoA18-q70fm_n_FbKynEEiTU_joWlTh",
-      accounts: [`d2536bf1124cba021fdaa9219963e90546b5f86dd46f16ad1cd49f8b4b1ef4db`]
+      url: "",
+      accounts: [``]
     }
   },
   etherscan: {
-    apiKey: "BM2EEY7TNR4UR51JXZPYVGPE43UMEJ1AC9"
+    apiKey: ""
   }
 };
